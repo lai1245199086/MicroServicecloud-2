132eac588124d2297747e5a808f724f0e599267c
llb1206 <llb1206>
2019-06-08 03:48:01 +0800

    Initial commit

---
+++ microservicecloud-provider-dept-8002/src/main/java/com/llb/springcloud/DeptProvider8002_App.java
@@ -0,0 +1,13 @@
+package com.llb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8002_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptProvider8002_App.class, args);
+	}
+}
